a7974c6fa5af60d09e371d68890e9ae41296b800
Adriano Cruz <92281726+adrianocrhuz@users.noreply.github.com>
2024-04-15 11:11:16 +0100

    aulas js

---
+++ 13 variaveis - 2/variaveis2.js
@@ -0,0 +1,20 @@
+let teste = "minha string"
+//console.log(teste)
+teste = 10
+console.log(teste)
+
+const NOME = 'Sophia'
+console.log(NOME)
+const teste2 = 10
+console.log(teste2)
+// variavel sem valor
+let nome;
+
+nome = "Sophia Cruz"
+console.log(nome)
+
+//no const tem que da valor a variavel
+const teste3 = 'eu sou imutavel'
+console.log(teste3)
+
+//ctrl + alt seta pra baixo ou seta pra cima
