a7094bd18b2827ae546c54d10d17b50299f5ecbd
Adriano Cruz <92281726+adrianocrhuz@users.noreply.github.com>
2024-05-17 09:34:36 +0100

    exerc js

---
+++ 30 curto-circuito/curto-circuito.js
@@ -0,0 +1,19 @@
+/*let n = 0
+if(!n) {
+    n = 10
+}
+console.log(n)
+*/
+/*
+let n = 1
+n = n || 10
+console.log(n)
+*/
+let isValid = false
+
+//if(isValid) {
+//    console.log("é valido")
+//}
+//isValid && console.log("é valido")
+
+isValid || console.log("não é valido")
