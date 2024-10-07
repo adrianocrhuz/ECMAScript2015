4d696c003aae7324e22b2681be3b1d12e0d237ab
Adriano Cruz <92281726+adrianocrhuz@users.noreply.github.com>
2024-10-07 09:07:41 +0100

    JS

---
+++ ex253/script.js
@@ -0,0 +1,5 @@
+function contarUmaHistoria(personagem = 'Branquinha', atividade = 'Correr no parque', nome_dono = 'Silvana') {
+    document.write(`Era uma vez um cachorro chamado ${personagem}, ele adorava ${atividade}, seu dono era o ${nome_dono}, e eles viveram felizes para sempre`)
+}
+//contarUmaHistoria('Tobi', 'Lati muito', 'Adriano')
+contarUmaHistoria(undefined,undefined,undefined)
---
+++ ex254/script.js
@@ -0,0 +1,42 @@
+/*
+let quadrado = function(x = 5) {
+    return x * x
+}
+document.write(quadrado(3))
+*/
+
+/*
+let quadrado = (x) => {
+    return x * x
+}
+document.write(quadrado(10))
+*/
+
+/*
+let quadrado = x => x * x //return implícito
+
+document.write(quadrado(8))
+*/
+
+/*
+let parOuImpar = function(numero) {
+    if (numero % 2 == 0) {
+        return "Par"
+    } else {
+        return "Impar"
+    }
+}
+document.write(parOuImpar(11))
+*/
+/*
+let parOuImpar = (numero) => {
+    if (numero % 2 == 0) {
+        return "Par"
+    } else {
+        return "Impar"
+    }
+}
+document.write(parOuImpar(47))
+*/
+let parOuImpar = numero => numero % 2 == 0 ? "Par" : "Impar"
+document.write(parOuImpar(38))
